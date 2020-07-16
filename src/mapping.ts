323cb8508ff091a7a4d1c9142ccea60a319db7d1
Max Alekseenko <nnwen0609@gmail.com>
2020-07-16 16:45:25 +0300

    add common data

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,8 +4,13 @@ import {
   Deposited,
   Withdrawn,
   WithdrawalRequested,
+  FeeSet,
+  WithdrawalLockDurationSet,
+  WithdrawalUnlockDurationSet,
+  TotalSupplyFactorSet,
+  SigmoidParametersSet,
 } from '../generated/Contract/Contract';
-import { Deposit } from '../generated/schema';
+import { Deposit, CommonData } from '../generated/schema';
 
 export function handleDeposited(event: Deposited): void {
   let id = event.params.sender.toHex() + '-' + event.params.id.toHex();
@@ -21,6 +26,7 @@ export function handleDeposited(event: Deposited): void {
   deposit.amount = event.params.balance;
   deposit.timestamp = event.block.timestamp;
   deposit.save();
+  updateTotalDeposited(event.address);
 }
 
 export function handleWithdrawn(event: Withdrawn): void {
@@ -33,6 +39,7 @@ export function handleWithdrawn(event: Withdrawn): void {
   let contract = Contract.bind(event.address);
   deposit.withdrawalRequestTimestamp = contract.withdrawalRequestsDates(event.params.sender, event.params.id);
   deposit.save();
+  updateTotalDeposited(event.address);
 }
 
 export function handleWithdrawalRequested(event: WithdrawalRequested): void {
@@ -40,4 +47,59 @@ export function handleWithdrawalRequested(event: WithdrawalRequested): void {
   let deposit = Deposit.load(id);
   deposit.withdrawalRequestTimestamp = event.block.timestamp;
   deposit.save();
+}
+
+function getCommonDataEntity(): CommonData | null {
+  let commonData = CommonData.load('common');
+  if (commonData == null) {
+    commonData = new CommonData('common');
+    commonData.sigmoidParamA = BigInt.fromI32(0);
+    commonData.sigmoidParamB = BigInt.fromI32(0);
+    commonData.sigmoidParamC = BigInt.fromI32(0);
+    commonData.fee = BigInt.fromI32(0);
+    commonData.withdrawalLockDuration = BigInt.fromI32(0);
+    commonData.withdrawalUnlockDuration = BigInt.fromI32(0);
+    commonData.totalSupplyFactor = BigInt.fromI32(0);
+    commonData.totalStaked = BigInt.fromI32(0);
+  }
+  return commonData;
+}
+
+function updateTotalDeposited(contractAddress: Address): void {
+  let commonData = getCommonDataEntity();
+  let contract = Contract.bind(contractAddress);
+  commonData.totalStaked = contract.totalStaked();
+  commonData.save();
+}
+
+export function handleFeeSet(event: FeeSet): void {
+  let commonData = getCommonDataEntity();
+  commonData.fee = event.params.value;
+  commonData.save();
+}
+
+export function handleWithdrawalLockDurationSet(event: WithdrawalLockDurationSet): void {
+  let commonData = getCommonDataEntity();
+  commonData.withdrawalLockDuration = event.params.value;
+  commonData.save();
+}
+
+export function handleWithdrawalUnlockDurationSet(event: WithdrawalUnlockDurationSet): void {
+  let commonData = getCommonDataEntity();
+  commonData.withdrawalUnlockDuration = event.params.value;
+  commonData.save();
+}
+
+export function handleTotalSupplyFactorSet(event: TotalSupplyFactorSet): void {
+  let commonData = getCommonDataEntity();
+  commonData.totalSupplyFactor = event.params.value;
+  commonData.save();
+}
+
+export function handleSigmoidParametersSet(event: SigmoidParametersSet): void {
+  let commonData = getCommonDataEntity();
+  commonData.sigmoidParamA = event.params.a;
+  commonData.sigmoidParamB = event.params.b;
+  commonData.sigmoidParamC = event.params.c;
+  commonData.save();
 }
